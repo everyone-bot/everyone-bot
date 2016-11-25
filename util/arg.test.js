68f85da3c1acbb7713fac4b7a40f757d7a0adc81
Aquib Master <aquibmaster@gmail.com>
2016-11-26 00:41:29 +1300

    Extend arg tests.
    + Ensure that we're throwing errors with friendly messages where the
    valName is supplied.

--- util/arg.test.js
+++ util/arg.test.js
@@ -13,6 +13,15 @@ test('checkIfExists should throw if the supplied value is undefined', t => {
     });
 });
 
+test('checkifExists should throw an error with a friendly message', t => {
+    const apple = undefined;
+    const error = t.throws(() => {
+        arg.checkIfExists(apple, 'apple');
+    });
+
+    t.is(error.message, 'apple does not exist');
+});
+
 test('checkIfExists should not throw if the supplied value is null', t => {
     t.notThrows(() => {
         arg.checkIfExists(null);
@@ -41,6 +50,15 @@ test('checkIfNullOrEmpty should throw if the supplied value is an empty string',
      t.throws(() => {
         arg.checkIfNullOrEmpty('');
      });
+});
+
+test('checkIfNullOrEmpty should throw an error with a friendly message', t => {
+    const username = '';
+    const error = t.throws(() => {
+        arg.checkIfNullOrEmpty(username, 'username');
+    });
+
+    t.is(error.message, 'username was null or empty');
 });
 
 test('checkIfNullOrEmpty should not throw if the supplied value is a truthy string', t => {
@@ -72,6 +90,16 @@ test('checkIfNumber should throw if the supplied value is null', t => {
         arg.checkIfNumber(null);
     });
 });
+
+test('checkIfNumber should throw an error with a friendly message', t => {
+    const age = '22';
+    const error = t.throws(() => {
+        arg.checkIfNumber(age, 'age');
+    });
+
+    t.is(error.message, 'age was not a number');
+});
+
 
 test('should not throw if the supplied value is a positive number', t => {
    t.notThrows(() => {
