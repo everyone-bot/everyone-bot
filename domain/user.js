8fa6264f52ef26adca257e422c73f1e122531c4d
Aquib Master <aquibmaster@gmail.com>
2016-11-30 17:28:57 +1300

    Add jsdocs for the User domain object

--- domain/user.js
+++ domain/user.js
@@ -1,6 +1,16 @@
 const arg = require('../util/arg');
 
+/**
+ * Entity to represent a telegram user.
+ */
 class User {
+    /**
+     * @param  {Number}
+     * @param  {String}
+     * @param  {String}
+     * @param  {String}
+     * @return {User}
+     */
     constructor(id, username, firstName, lastName) {
         arg.checkIfNumber(id, 'id');
         arg.checkIfNullOrEmpty(username, 'username');
@@ -13,18 +23,30 @@ class User {
         this._lastName = lastName;
     }
 
+    /**
+     * @return {Number}
+     */
     get id() {
         return this._id;
     }
 
+    /**
+     * @return {String}
+     */
     get username() {
         return this._username;
     }
 
+    /**
+     * @return {String}
+     */
     get firstName() {
         return this._firstName;
     }
 
+    /**
+     * @return {String}
+     */
     get lastName() {
         return this._lastName;
     }
