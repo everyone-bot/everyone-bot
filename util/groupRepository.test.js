c2461062fad3d635376d194bbd3904e10672171d
Aquib Master <aquibmaster@gmail.com>
2016-12-06 08:16:05 +1300

    Write opt-in tests

--- util/groupRepository.test.js
+++ util/groupRepository.test.js
@@ -2,15 +2,42 @@ import test from 'ava';
 import nock from 'nock';
 
 const FirebaseSettings = require('../domain/firebaseSettings');
+const User = require('../domain/user');
 const GroupRepository = require('./groupRepository');
 
-test.beforeEach(t => {
-    let firebaseSettings = new FirebaseSettings('example', 'database-key');
+const optInMock = nock('https://example.firebaseio.com')
+    .patch('/groups/1234/members/1.json?auth=database-key')
+    .reply(200);
+
+const fakeUser = new User(1, 'aquibm');
+const firebaseSettings = new FirebaseSettings('example', 'database-key');
+
+test.beforeEach(async t => {
     t.context.repo = new GroupRepository(firebaseSettings);
 });
 
-test('should be able to opt users in', t => {
-    
+test('optIn should throw if user is undefined', async t => {
+    t.throws(() => {
+        t.context.repo.optIn(undefined, 1234);
+    });
 });
 
-nock('https://example.firebaseio.com').patch('/group/1234/members/1.json').reply(200);
+test('optIn should throw if groupId is undefined', async t => {
+    t.throws(() => {
+        t.context.repo.optIn(fakeUser);
+    });
+});
+
+test('optIn should throw if groupId is not a number', async t => {
+    t.throws(() => {
+        t.context.repo.optIn(fakeUser, '1234');
+    });
+});
+
+test('optin', async t => {
+    t.plan(1);
+
+    // TODO(AM): This expects a & at the end of the query params. Fix.
+    await t.context.repo.optIn(fakeUser, 1234);
+    t.is(optInMock.isDone(), true);
+});
