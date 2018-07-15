cdddc61d06955db57fe0b1ebcde3d69645c1376e
Aquib Master <aquibmaster@gmail.com>
2018-07-15 23:22:33 +1200

    Switch over to telegraf

--- index.js
+++ index.js
@@ -1,31 +1,37 @@
-'use strict';
+'use strict'
 
-const tg = require('telegram-node-bot');
-const SettingsRepository = require('./util/settingsRepository');
-const GroupRepository = require('./util/groupRepository');
-const StatisticsRepository = require('./util/statisticsRepository');
-const MentionBuilder = require('./util/mentionBuilder');
-const FirebaseSettings = require('./domain/firebaseSettings');
-const EveryoneController = require('./controllers/everyoneController.js');
-const OnboardingController = require('./controllers/onboardingController.js');
+const Telegraf = require('telegraf')
+const SettingsRepository = require('./util/settingsRepository')
+const GroupRepository = require('./util/groupRepository')
+const StatisticsRepository = require('./util/statisticsRepository')
+const MentionBuilder = require('./util/mentionBuilder')
+const FirebaseSettings = require('./domain/firebaseSettings')
 
-const config = !process.env.PRODUCTION && require('./config.json');
-const settings = new SettingsRepository(config);
+const config = !process.env.PRODUCTION && require('./config.json')
+const settings = new SettingsRepository(config)
 
-const mentionBuilder = new MentionBuilder(settings);
-const firebaseSettings = new FirebaseSettings(settings.firebaseProjectName, settings.firebaseDatabaseSecret);
-const groupRepository = new GroupRepository(firebaseSettings);
-const statisticsRepository = new StatisticsRepository(firebaseSettings);
+const mentionBuilder = new MentionBuilder(settings)
+const firebaseSettings = new FirebaseSettings(
+    settings.firebaseProjectName,
+    settings.firebaseDatabaseSecret,
+)
+const groupRepository = new GroupRepository(firebaseSettings)
+const statisticsRepository = new StatisticsRepository(firebaseSettings)
 
-const bot = new tg.Telegram(settings.telegramApiKey, {
-    workers: settings.botWorkers,
-    webAdmin: {
-        port: process.env.PORT || 7777
-    }
-});
+const bot = new Telegraf(settings.telegramApiKey, {
+    username: settings.botUsername,
+})
 
-bot.router
-    .when(new tg.TextCommand('/everyone', 'everyone'), new EveryoneController(groupRepository, mentionBuilder, statisticsRepository))
-    .when(new tg.TextCommand('/in', 'in'), new EveryoneController(groupRepository, mentionBuilder, statisticsRepository))
-    .when(new tg.TextCommand('/out', 'out'), new EveryoneController(groupRepository, mentionBuilder, statisticsRepository))
-    .when(new tg.TextCommand('/start', 'start'), new OnboardingController());
+const onboardingController = require('./controllers/onboardingController')
+const everyoneController = require('./controllers/everyoneController')(
+    groupRepository,
+    mentionBuilder,
+    statisticsRepository,
+)
+
+bot.command('everyone', everyoneController.everyone)
+bot.command('in', everyoneController.in)
+bot.command('out', everyoneController.out)
+bot.command('start', onboardingController.start)
+
+bot.startPolling()
--- util/settingsRepository.js
+++ util/settingsRepository.js
@@ -7,43 +7,51 @@ class SettingsRepository {
      * @return {SettingsRepository}
      */
     constructor(config) {
-        this._config = config;
+        this._config = config
     }
 
     /**
      * @return {String}
      */
     get telegramApiKey() {
-        return process.env.TELEGRAM_API_KEY || this._config.telegramApiKey;
+        return process.env.TELEGRAM_API_KEY || this._config.telegramApiKey
     }
 
     /**
-     * @return {Number}
+     * @return {String}
      */
-    get botWorkers() {
-        return process.env.BOT_WORKERS ? Number.parseInt(process.env.BOT_WORKERS, 10) : this._config.botWorkers;
+    get botUsername() {
+        return process.env.BOT_USERNAME || this._config.botUsername
     }
 
     /**
      * @return {String}
      */
     get firebaseProjectName() {
-        return process.env.FIREBASE_PROJECT_NAME || this._config.firebaseProjectName;
+        return (
+            process.env.FIREBASE_PROJECT_NAME ||
+            this._config.firebaseProjectName
+        )
     }
 
     /**
      * @return {String}
      */
     get firebaseDatabaseSecret() {
-        return process.env.FIREBASE_DATABASE_SECRET || this._config.firebaseDatabaseSecret;
+        return (
+            process.env.FIREBASE_DATABASE_SECRET ||
+            this._config.firebaseDatabaseSecret
+        )
     }
 
     /**
      * @return {string}
      */
     get mentionsPerMessage() {
-        return process.env.MENTIONS_PER_MESSAGE ? Number.parseInt(process.env.MENTIONS_PER_MESSAGE, 10) : this._config.mentionsPerMessage;
+        return process.env.MENTIONS_PER_MESSAGE
+            ? Number.parseInt(process.env.MENTIONS_PER_MESSAGE, 10)
+            : this._config.mentionsPerMessage
     }
 }
 
-module.exports = SettingsRepository;
+module.exports = SettingsRepository
