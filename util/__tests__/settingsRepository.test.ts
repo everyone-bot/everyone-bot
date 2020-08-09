373b4941ecfb53a624218e33b4d6e7f0020fe4da
Aquib Master <aquibmaster@gmail.com>
2020-08-09 12:15:30 +1200

    Convert settings repo

---
+++ util/__tests__/settingsRepository.test.ts
@@ -0,0 +1,41 @@
+import SettingsRepository, { Config } from '../settingsRepository'
+
+describe('Settings repository tests', () => {
+    const stubConfig: Config = {
+        telegramApiKey: 'TELEGRAM_API_KEY',
+        botUsername: 'EVERYONE',
+        firebaseProjectName: 'FIREBASE_PROJECT_NAME',
+        firebaseDatabaseSecret: 'FIREBASE_DATABASE_SECRET',
+        mentionsPerMessage: 5,
+        enableRemoveInactiveMembersCommand: false
+    }
+
+    it('should return config values when environment variables don\'t exist', () => {
+        const settings = new SettingsRepository(stubConfig)
+
+        expect(settings.telegramApiKey).toEqual(stubConfig.telegramApiKey)
+        expect(settings.botUsername).toEqual(stubConfig.botUsername)
+        expect(settings.firebaseProjectName).toEqual(stubConfig.firebaseProjectName)
+        expect(settings.firebaseDatabaseSecret).toEqual(stubConfig.firebaseDatabaseSecret)
+        expect(settings.mentionsPerMessage).toEqual(stubConfig.mentionsPerMessage)
+        expect(settings.enableRemoveInactiveMembersCommand).toEqual(stubConfig.enableRemoveInactiveMembersCommand)
+    })
+
+    it('should convert and prefer env vars when they do exist', () => {
+        const settings = new SettingsRepository(stubConfig)
+
+        process.env.TELEGRAM_API_KEY = 'env-telegram-api-key'
+        process.env.BOT_USERNAME = 'ENV_EVERYONE'
+        process.env.FIREBASE_PROJECT_NAME = 'env-firebase-project-name'
+        process.env.FIREBASE_DATABASE_SECRET = 'env-firebase-database-secret'
+        process.env.MENTIONS_PER_MESSAGE = '1'
+        process.env.ENABLE_REMOVE_INACTIVE_MEMBERS_COMMAND = 'true'
+
+        expect(settings.telegramApiKey).toEqual(process.env.TELEGRAM_API_KEY)
+        expect(settings.botUsername).toEqual(process.env.BOT_USERNAME)
+        expect(settings.firebaseProjectName).toEqual(process.env.FIREBASE_PROJECT_NAME)
+        expect(settings.firebaseDatabaseSecret).toEqual(process.env.FIREBASE_DATABASE_SECRET)
+        expect(settings.mentionsPerMessage).toEqual(1)
+        expect(settings.enableRemoveInactiveMembersCommand).toBeTruthy()
+    })
+})
